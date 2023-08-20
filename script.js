5359b6e83aaf2b5208b122bc9048deccd8605db1
Jack Sharkey <113288400+Sharkey0110@users.noreply.github.com>
2023-08-20 23:14:31 +0100

    Completing the basic HTML and CSS of the project

---
+++ script.js
@@ -0,0 +1,4 @@
+const display = document.querySelector("#myLabel");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
